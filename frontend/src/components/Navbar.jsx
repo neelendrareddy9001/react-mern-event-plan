cf1a05be787df2736876f058b0222b4e3c49818a
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-12-17 23:08:43 +0100

    navbar completed styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
-
   return (
-    <nav className="w-full p-3 bg-gray-400 text-white">
+    <nav>
       <div className="logo">KING's</div>
-      <div>
-        <Link to="/hero" spy={true} smooth={true} duration={500}>
-          HOME
-        </Link>
-        <Link to="/services" spy={true} smooth={true} duration={500}>
-          SERVICES
-        </Link>
-        <Link to="/about" spy={true} smooth={true} duration={500}>
-          ABOUT
-        </Link>
-        <Link to="/contact" spy={true} smooth={true} duration={500}>
-          CONTACT
-        </Link>
+      <div className={show ? "navLinks showmenu" : "navLinks"}>
+        <div className="links">
+          <Link to="hero" spy={true} smooth={true} duration={500}>
+            HOME
+          </Link>
+          <Link to="services" spy={true} smooth={true} duration={500}>
+            SERVICES
+          </Link>
+          <Link to="about" spy={true} smooth={true} duration={500}>
+            ABOUT
+          </Link>
+          <Link to="contact" spy={true} smooth={true} duration={500}>
+            CONTACT
+          </Link>
+        </div>
+      </div>
+      <div className="hamburger" onClick={() => setShow(!show)}>
+        <GiHamburgerMenu />
       </div>
     </nav>
   );
